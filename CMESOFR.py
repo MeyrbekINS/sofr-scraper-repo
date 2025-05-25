e2c555fd3936d8577afa4bdac71ccce12bed8041
MeyrbekINS <mejrbekturan@gmail.com>
2025-05-25 07:48:01 +0500

    Update CMESOFR.py

--- CMESOFR.py
+++ CMESOFR.py
@@ -59,11 +59,17 @@ def fetch_sofr_strip_rates_with_selenium():
         chrome_options.add_argument('--window-size=1920,1080') # Can help with some headless issues
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
+        chrome_options.add_argument('--user-data-dir=/tmr/user-data')
 
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        
         driver = webdriver.Chrome(service=service, options=chrome_options)
         print("WebDriver Manager successfully set up ChromeDriver (headless mode).")
     except Exception as e_manager:
+        import traceback
         print(f"Error setting up ChromeDriver with WebDriver Manager: {e_manager}")
+        traceback.print_exc()
         return pd.DataFrame()
 
     extracted_data_df = pd.DataFrame()
